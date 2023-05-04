162d5c185775cf16a6fdfba08de558fcdb29d09a
Yazdan <yzdn.jt@gmail.com>
2023-05-04 20:12:20 +0330

    Update models/index.js -> adding new collections

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,13 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-// db.tutorials = require("./tutorial.model.js")(mongoose);
+
 db.users = require("./users.model.js")(mongoose);
+db.students = require("./students.model.js");
+db.teachers = require("./teachers.model.js");
+db.edu_managers = require("./edu_manager.model.js");
+db.basic_lesssons = require("./basic_lessons.model.js");
+db.termic_lessons = require("./termic_lessons.model.js");
+
 
 module.exports = db;
