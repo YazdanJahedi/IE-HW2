430e689592297590495ec401ef42ca9451e0462f
Yazdan <yzdn.jt@gmail.com>
2023-05-04 20:09:16 +0330

    Add basic/termic lessons as new models

---
+++ src/models/termic_lessons.model.js
@@ -0,0 +1,17 @@
+const Basic_lessons = require("./users.model");
+
+module.exports = mongoose => {
+    const Termic = Basic_lessons.discriminator('Termic_lessons', 
+        new mongoose.Schema({
+            class_date: Date,
+            class_time: String,
+            exam_date: Date,
+            exam_time: String,
+            exam_loc: String,
+            teacher_name: String,
+            capasity: Number,
+            semister: Number,
+        })
+    );
+    return Termic;
+};
