3af4c3af6c9c2d89e3e2d97a1fd1eb0325e7ddb1
Yazdan <yzdn.jt@gmail.com>
2023-05-14 02:06:11 +0330

    Refactor auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,60 +1,49 @@
-const config = require("../config/auth.config.js");
+const auth_config = require("../config/auth.config.js");
 const db = require("../models");
-const User = db.users;
-const tokenDb = db.token;
-
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcrypt");
 
 
-exports.sign_in = (req,res)=>{
-    console.log("login req resiaved...")
+exports.login = (req,res)=>{
+    console.log("login request...")
 
-    User.findOne({
-        user_id: req.body.user_id
-    }).then( 
-      user => {
-        if (!user) {
-            return res.status(404).send({ message : "user not found" });
+    db.users.findOne(
+        {
+            user_id: req.body.user_id
         }
+    )
+    .then(user => {
+        if (!user) 
+            return res.status(404).send( { message : "fail: user not found" } );
         
-        console.log("login... user_id found...")
-        var passIsValid = bcrypt.compareSync(req.body.password,user.password);
-        if (!passIsValid){
-            return res.status(401).send( { message: "pass is incorrect" } );
-        }
-        console.log("login... pass is ok...")
-        var ntoken = jwt.sign({id : user._id}, config.secret, {
-            expiresIn : 600,
-        });
-        req.session.token = ntoken;
-        const tokenObj = new tokenDb({
+        // check if password is valid or not
+        if (!bcrypt.compareSync(req.body.password, user.password))
+            return res.status(401).send( { message: "fail: Password is not true" } );
+        
+        const the_token = jwt.sign({id: user._id}, auth_config.secret, {expiresIn: 600});
+        
+        const new_token = new db.token({
             user_id : user._id,
-            token : ntoken,
-            type : user.__t // type
+            token : the_token,
+            type : user.__t  // type
         });
-        if (tokenObj == null){
-            console.log("tokenObg is null")
-            res.status(400).send({
-                message : "an error accured adding token to db"
-            })
-        }else{
-            tokenObj.save().then(()=>{
-                console.log("login... save tokenObj")
-                res.status(200).send({
-                    id : user._id,
-                    user_id : user.user_id,
-                    email : user.email,
-                    type : user.__t  // type
-                });
-            }).catch(
-                (err) => {
-                    res.status(500).send({ message: err || "token is not saved" }); 
-                }
-            )
-        }
-    }).catch((err)=>{
-        res.status(500).send({ message: err || "user not found" });
-        return;
+
+        req.session.token = the_token;
+
+        new_token.save()
+        .then(() => {
+            res.status(200).send({
+                id : user._id,
+                user_id : user.user_id,
+                type : user.__t 
+            });
+        })
+        .catch((err) => {
+                res.status(500).send({ message: "fail: token is not saved" || err}); 
+            }
+        )
+    })
+    .catch((err) => {
+        res.status(500).send({ message: "fail: user not found" || err});
     });
 }
